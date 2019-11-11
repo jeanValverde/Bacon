27e2e54d5bb43f5098ce1676a59f74b06484c3dc
Jean Gonzalez <jea.gonzalezv@alumnos.duoc.cl>
2019-11-11 08:43:06 -0300

    Deploy Mesas, Pedidos

--- src/main/java/com/restaurante/bacon/dto/EstadoPedido.java
+++ src/main/java/com/restaurante/bacon/dto/EstadoPedido.java
@@ -13,13 +13,10 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 /**
