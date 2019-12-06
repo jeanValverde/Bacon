090d35ee622a782faf606830943ddd9cd375c6c8
Jean Gonzalez <jea.gonzalezv@alumnos.duoc.cl>
2019-12-06 13:07:07 -0300

    Deploy

--- src/main/java/com/restaurante/bacon/controller/APIpago.java
+++ src/main/java/com/restaurante/bacon/controller/APIpago.java
@@ -5,8 +5,15 @@
  */
 package com.restaurante.bacon.controller;
 
+import com.restaurante.bacon.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -15,5 +22,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/API")
 @Controller
 public class APIpago {
+     @Autowired
+    ClienteService clienteService;
     
+    @PostMapping("/pago")
+    public ResponseEntity pagarOrdenes(Model modelo, @RequestParam("order_id") Integer order_id, @RequestParam("transaction_id") Integer transaction_id, 
+            @RequestParam("status") Integer status, @RequestParam("verification_key") String verification_key) {
+        
+        System.out.println(order_id);
+        System.out.println(transaction_id);
+        System.out.println(status);
+        System.out.println(verification_key);
+        
+        if(status == 1){
+             Integer result = this.clienteService.pagarOnline(order_id);
+        }
+        return new ResponseEntity(true, HttpStatus.OK);
+    }
+    
+    @PostMapping("/prueba")
+    public ResponseEntity prueba(Model modelo, @RequestParam("order_id") Integer order_id, @RequestParam("transaction_id") Integer transaction_id, 
+            @RequestParam("status") Integer status, @RequestParam("verification_key") String verification_key) {
+        
+        if(status == 1){
+             Integer result = this.clienteService.pagarOnline(order_id);
+        }
+        return new ResponseEntity(true, HttpStatus.OK);
+    }
 }
